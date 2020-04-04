e61c52879f310a7417a33fd7c59f0fb47e910c1a
brianferrao <shirva.brian@gmail.com>
2020-04-03 23:21:02 -0400

    Props with children

--- src/index.js
+++ src/index.js
@@ -2,26 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetails from './CommentDetails';
 import faker from 'faker';
+import ApprovalCard from './ApprovalCard';
 
 
 const App = () => {
     return (
-        <div className="ui comments">
-            <CommentDetails 
-                author="Brian" 
-                avatar={faker.image.avatar()} 
-                postedOn="Today at 4:00PM"
-                comment="Awesome Post!!" />
-            <CommentDetails 
-                author="Veva" 
-                avatar={faker.image.avatar()} 
-                postedOn="Today at 2:00PM"
-                comment="Awesome Post Again!!" />
-            <CommentDetails 
-                author="Leannah" 
-                avatar={faker.image.avatar()} 
-                postedOn="Today at 1:00AM"
-                comment="Awesome Post ??!!" />
+        <div className="ui container  comments">
+            <ApprovalCard>
+                <CommentDetails 
+                    author="Brian" 
+                    avatar={faker.image.avatar()} 
+                    postedOn="Today at 4:00PM"
+                    comment="Awesome Post!!" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetails 
+                    author="Veva" 
+                    avatar={faker.image.avatar()} 
+                    postedOn="Today at 2:00PM"
+                    comment="Awesome Post Again!!" />
+            </ApprovalCard>            
+            <ApprovalCard>
+                <CommentDetails 
+                    author="Leannah" 
+                    avatar={faker.image.avatar()} 
+                    postedOn="Today at 1:00AM"
+                    comment="Awesome Post ??!!" />
+            </ApprovalCard>            
         </div>
     );
 };
